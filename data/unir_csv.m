ac0267b87b01ecd112c41ee2f5dd025e10d46ead
gerald_xyz <gerald.cainicela.a@gmail.com>
2025-07-22 11:51:52 -0500

    Update unir.csv

--- data/unir_csv.m
+++ data/unir_csv.m
@@ -2,9 +2,10 @@ clc, clear;
 T1 = readtable('data_data_alverjita_verde_partida.csv');
 T2 = readtable('data_data_garbanzo.csv');
 T3 = readtable('data_data_frejol_canario.csv');
+T4 = readtable('data_data_frejol_rojo.csv');
 %T3 = readtable('data_frijoles.csv'); % si tienes más clases
 
-T = [T1; T2; T3]; % Unir todas las tablas
+T = [T1; T2; T3; T4]; % Unir todas las tablas
 T = T(randperm(height(T)), :);
 
 writetable(T, 'data_legumbres.csv'); % Guardar combinado
